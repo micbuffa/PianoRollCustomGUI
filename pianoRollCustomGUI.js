b78c34bc526b3356725ee56e6a4a539af61eb772
Michel Buffa <michelbuffa@macbook-pro-de-michel.home>
2025-04-09 11:45:42 +0200

    fist commit

---
+++ pianoRollCustomGUI.js
@@ -0,0 +1,235 @@
+// class to describe a cell with row, col, color
+class PianoRollCustomGUICell {
+    constructor(col, row, color) {
+        this.col = col;
+        this.row = row;
+        this.color = color;
+        this.active = false;
+    }
+}
+
+class PianoRollCustomGUI {
+    constructor(nbrows, nbBars, timeSigNumerator, timeSigDenominator) {
+        this.cells = [];
+        // number of rows and columns depending on the number of bars and time signature
+        // number of columns is nbBars * timeSigNumerator * timeSigDenominator
+        this.cols = nbBars * timeSigNumerator * timeSigDenominator;
+        this.rows = nbrows //  rows for the piano roll
+        this.w = width / this.cols;
+        this.h = height / this.rows;
+        this.createCells();
+        this.playheadPosition = 100;
+        this.playheadColor = color(0, 255, 0);
+        this.nbBars = 1;
+        this.timeSignatureDenominator = timeSigDenominator; // 4 subdivisions per bar
+        this.timeSignatureNumerator = timeSigNumerator; // 4 beats per bar
+        this.tempo = 60; // in BPM
+        // the GUI displays nb bars of timeSignatureNumerator beats
+        // each beat is made of timeSignatureDenominator subdivisions
+        // ONE CELL IS ONE SUBDIVISION !
+        // NUMBER OF SUBDIVISIONS PER BEAT = timeSignatureDenominator
+        // NUMBER OF BEATS IN ONE BAR = timeSignatureNumerator
+        // NUMBER OF SUBDIVISIONS IN ONE BAR = timeSignatureDenominator * timeSignatureNumerator
+        // time to play one beat is 60 / tempo = 1s if the tempo is 60 BPM
+        // so the playhead moves 1 beat every second at 60 BPM
+        // it moves 4 cells (subdivisions) every second at 60 BPM if the time signature is 4/4
+        // the speed in pixels per second is the number of cells in one bar
+        // divided by the time in seconds to play one bar
+        this.timePerSecondToPlayOneBar = (this.timeSignatureDenominator * this.nbBars) / this.tempo; // in seconds
+        console.log(this.timePerSecondToPlayOneBar);
+        // beat subdivision (one cell) duration in milliseconds
+        this.beatSubdivisionDuration = (60 / this.tempo) * 1000 / this.timeSignatureDenominator; // in milliseconds
+        this.started = false;
+    }
+
+    createCells() {
+        // create the cells, taking into account the number rows and cols
+        for (let row = 0; row < this.rows; row++) {
+            for (let col = 0; col < this.cols; col++) {
+                const cell = new PianoRollCustomGUICell(col, row, color(255));
+                this.cells.push(cell);
+            }
+        }
+    }
+    updateCell(col, row, color) {
+        const cell = this.cells.find(c => c.col === col && c.row === row);
+        if (cell) {
+            cell.color = color;
+        }
+    }
+    draw() {
+        // Draw the cells
+        for (let i = 0; i < this.cells.length; i++) {
+            const cell = this.cells[i];
+            fill(cell.color);
+            stroke(0);
+            rect(cell.col * this.w, cell.row * this.h, this.w, this.h);
+        }
+
+        // draw thick lines every beat
+        push();
+        stroke(0);
+        strokeWeight(4);
+        // draw a line every timeSignatureDenominator cells
+        for (let i = 0; i < this.cols; i += this.timeSignatureDenominator) {
+            line(i * this.w, 0, i * this.w, height);
+        }
+        pop();
+
+        // draw super thick lines every bar
+        push();
+        stroke(0);
+        strokeWeight(8);
+        for (let i = 0; i < this.cols; i += this.timeSignatureDenominator * this.timeSignatureNumerator) {
+            line(i * this.w, 0, i * this.w, height);
+        }
+        pop();
+
+        if (this.started) {            
+            // compute playhead position depending on the time elapsed since start (in milliseconds)
+            //console.log(this.timeElaspedSinceStart);
+            this.playheadPosition = (this.timeElaspedSinceStart / this.beatSubdivisionDuration) % this.cols;
+            //this.playheadPosition += inc;
+            if (this.playheadPosition > this.cols) {
+                this.playheadPosition = 0;
+            }
+            // Draw the playhead
+            this.drawPlayhead();
+
+            // time elapsed since start in milliseconds with high accuracy. We could have used the 
+            // deltaTime global variable from p5js (also available in BabylonJS) to compute
+            // the elasped time with summing the deltas, but I prefer to use audioContext.currentTime
+            // so elapsed time = currentTime - startTime, as we're web audio based, and the pianoRoll WAM
+            // uses this has its main clock
+            //this.timeElaspedSinceStart += deltaTime;
+            this.timeElaspedSinceStart = (audioContext.currentTime * 1000) - this.startTime;
+            //console.log(this.timeElaspedSinceStart + " " + this.timeElaspedSinceStart);
+        }
+    }
+
+    // Draw the playhead
+    drawPlayhead() {
+        fill(this.playheadColor);
+        stroke(0);
+        rect(this.playheadPosition * this.w, 0, this.w / 10, height);
+        // Get column number from the playhead position
+        const col = Math.floor(this.playheadPosition);
+        // check if there are any cell activated in this column, and change its color to green
+        for (let i = 0; i < this.rows; i++) {
+            const cell = this.getCell(col, i);
+            if (this.isCellActive(cell)) {
+                console.log("cell active");
+                // Make it flash green for a short time and go back to red
+                cell.color = color(0, 255, 0);
+
+                setTimeout(() => {
+                    cell.color = color(255, 0, 0);
+                }, this.beatSubdivisionDuration);
+            }
+
+        }
+    }
+
+    setTempo(tempo) {
+        this.tempo = tempo;
+        // adjust the speed of the playhead
+        this.playHeadSpeedIncrement = (this.cols / this.nbBars * this.timeSignatureNumerator) / ((60 / this.tempo) * 1000); // in cells per second
+        // adjust the duration of one cell
+        this.beatSubdivisionDuration = (60 / this.tempo) * 1000 / this.timeSignatureDenominator; // in milliseconds
+    }
+
+    start() {
+        this.started = true;
+        this.playheadPosition = 0;
+        this.timeElaspedSinceStart = 0;
+        this.startTime = audioContext.currentTime * 1000; // in milliseconds
+        //console.log(this.timeElaspedSinceStart);
+    }
+
+    stop() {
+        this.started = false;
+        this.playheadPosition = 0;
+    }
+
+    clear() {
+        this.cells.forEach(cell => {
+            cell.color = color(255);
+        });
+    }
+    getCell(col, row) {
+        // returns the cell at the given column and row, taking into account the number of rows and columns
+        return this.cells.find(cell => cell.col === col && cell.row === row);
+    }
+    getCellIndex(col, row) {
+        return this.cells.findIndex(cell => cell.col === col && cell.row === row);
+    }
+    getCellColor(col, row) {
+        const cell = this.getCell(col, row);
+        return cell ? cell.color : null;
+    }
+    setCellColor(col, row, color) {
+        const cell = this.getCell(col, row);
+        if (cell) {
+            cell.color = color;
+        }
+    }
+
+    setCellActive(col, row) {
+        const cell = this.getCell(col, row);
+        if (cell) {
+            cell.color = color(255, 0, 0);
+            cell.active = true;
+        }
+    }
+
+    setCellInactive(col, row) {
+        const cell = this.getCell(col, row);
+        if (cell) {
+            cell.color = color(255);
+            cell.active = false;
+        }
+    }
+
+    toggleCellActive(cell) {
+        if (cell.active) {
+            this.setCellInactive(cell.col, cell.row);
+        } else {
+            this.setCellActive(cell.col, cell.row);
+        }
+    }
+
+    toggleCellColor(col, row) {
+        const cell = this.getCell(col, row);
+        if (cell) {
+            console.log(cell.color)
+            cell.color = (cell.color.levels[2] === 255) ? color(255, 0, 0) : color(255);
+        }
+    }
+
+    isCellActive(cell) {
+        // returns true if the cell is red
+        return cell.active
+    }
+
+    isCellEmpty(cell) {
+        return !cell.active;
+    }
+
+
+    getCellPosition(col, row) {
+        const cell = this.getCell(col, row);
+        return cell ? { x: cell.col * this.w, y: cell.row * this.h } : null;
+    }
+    getCellSize() {
+        return { width: this.w, height: this.h };
+    }
+    getCellCount() {
+        return this.cells.length;
+    }
+    getRowCount() {
+        return this.rows;
+    }
+    getColCount() {
+        return this.cols;
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,190 @@
+const audioContext = new AudioContext();
+let pianoRollCustomGUI;
+let currentBpm = 60;
+
+async function setup() {
+  createCanvas(1600, 800);
+
+  const hostGroupId = await setupWamHost();
+  // First WAM: a piano roll for generating notes, sort of step sequencer
+  const wamURIPianoRoll = "https://www.webaudiomodules.com/community/plugins/burns-audio/pianoroll/index.js";
+  const pianoRollInstance = await loadDynamicComponent(wamURIPianoRoll, hostGroupId);
+  const pianoRollDiv = await pianoRollInstance.createGui();
+  showWam(pianoRollDiv, 10, 50, 0.7, 500, 540);
+
+  // Second WAM: a synth to play the notes
+  // Example with a web assembly instrument (Pro24 synth) WAM compiled from C-Major code)
+  const wamURISynth = 'https://wam-4tt.pages.dev/Pro54/index.js';
+  synthInstance = await loadDynamicComponent(wamURISynth, hostGroupId);
+
+  // Display the WAM GUI (optionnal, WAMs can be used without GUI)
+  const synthDiv = await synthInstance.createGui();
+  showWam(synthDiv, 370, 50, 0.7);
+
+  // Build the audio graph
+  // a WAM is always handled like a single Web Audio node, even if
+  // its made internally of many nodes
+  synthInstance.audioNode.connect(audioContext.destination);
+
+  // build the "MIDI graph" (connect the pianoRoll to the synth)
+  pianoRollInstance.audioNode.connectEvents(synthInstance.instanceId);
+
+  // Create the pianoRoll custom GUI
+  // parameters: number of rows, number of bars, time signature numerator, time signature denominator
+  pianoRollCustomGUI = new PianoRollCustomGUI(12, 2, 4, 4);
+
+  const startButton = document.querySelector("#btn-start");
+  startButton.onclick = () => {
+    console.log(audioContext.state)
+    if (audioContext.state !== "running") {
+      audioContext.resume();
+    }
+
+    if (startButton.textContent === "Start") {
+      pianoRollInstance.audioNode.scheduleEvents({
+        type: 'wam-transport', data: {
+          playing: true,
+          timeSigDenominator: 4,
+          timeSigNumerator: 4,
+          currentBar: 0,
+          currentBarStarted: audioContext.currentTime,
+          tempo: currentBpm
+        }
+      });
+      // start custom GUI
+      pianoRollCustomGUI.start();
+
+      startButton.textContent = "Stop";
+    } else {
+
+      pianoRollInstance.audioNode.scheduleEvents({
+        type: 'wam-transport', data: {
+          playing: false,
+          timeSigDenominator: 4,
+          timeSigNumerator: 4,
+          currentBar: 0,
+          currentBarStarted: audioContext.currentTime,
+          tempo: currentBpm
+        }
+      });
+
+      // stop custom GUI
+      pianoRollCustomGUI.stop();
+
+      startButton.textContent = "Start";
+      // not mandatory. If present, allows a "play/pause" behavior.
+      // if not, presing start will start from beginning each time.
+      audioContext.suspend();
+    }
+  }
+
+  // Listener for bpm change
+  const bpmInput = document.querySelector("#bpm");
+  bpmInput.oninput = (e) => {
+    const bpm = parseInt(e.target.value);
+    console.log(bpm);
+    currentBpm = bpm;
+
+    // adjust pianoRoll tempo
+    pianoRollInstance.audioNode.scheduleEvents({
+      type: 'wam-transport', data: {
+        playing: true,
+        timeSigDenominator: 4,
+        timeSigNumerator: 4,
+        currentBar: 0,
+        currentBarStarted: audioContext.currentTime,
+        tempo: bpm
+      }
+    });
+
+    // adjust tempo of the pianoRoll custom GUI
+    pianoRollCustomGUI.setTempo(bpm);
+  }
+}
+
+function draw() {
+  // Let's draw our own piano roll as a matrix of square pads
+  if (pianoRollCustomGUI) {
+    pianoRollCustomGUI.draw();
+  }
+}
+
+
+
+// check click on a piano roll cell and turn it red/white on/off on click
+function mousePressed() {
+  const cols = pianoRollCustomGUI.cols;
+  const rows = pianoRollCustomGUI.rows;
+  const w = width / cols;
+  const h = height / rows;
+
+  const col = Math.floor(mouseX / w);
+  const row = Math.floor(mouseY / h);
+  const cell = pianoRollCustomGUI.getCell(col, row);
+    // Change color of the cell
+    pianoRollCustomGUI.toggleCellActive(cell);
+  
+}
+//-------------------
+// Utility functions 
+//-------------------
+async function setupWamHost() {
+  // Init WamEnv, load SDK etc.
+  const { default: initializeWamHost } = await import("https://www.webaudiomodules.com/sdk/2.0.0-alpha.6/src/initializeWamHost.js");
+  const [hostGroupId] = await initializeWamHost(audioContext);
+
+  // hostGroupId is useful to group several WAM plugins together....
+  return hostGroupId;
+}
+
+async function loadDynamicComponent(wamURI, hostGroupId) {
+  try {
+
+    // Import WAM
+    const { default: WAM } = await import(wamURI);
+    // Create a new instance of the plugin, pass groupId
+    const wamInstance = await WAM.createInstance(hostGroupId, audioContext);
+
+    return wamInstance;
+  } catch (error) {
+    console.error('Erreur lors du chargement du Web Component :', error);
+  }
+}
+
+function showWam(wamGUI, x, y, scale, width, height) {
+  // Create a container around the wam, so that we can rescale/position it easily
+  // this is where you can add a menu bar, close button, etc.
+  const container = document.createElement('div');
+  container.style.position = 'absolute';
+
+  container.style.overflow = 'auto';
+  container.style.zIndex = '10';  // above canvas
+
+  // Put the wam in the div
+  container.appendChild(wamGUI);
+
+  adjustPositionAndSize(container, x, y, scale);
+  if (height !== undefined)
+    container.style.height = height + "px";
+  if (width !== undefined)
+    container.style.width = width + "px";
+
+  document.body.appendChild(container);
+}
+
+function adjustPositionAndSize(wamContainer, x, y, scale) {
+  // rescale without changing the top left coordinates
+  wamContainer.style.transformOrigin = '0 0';
+  wamContainer.style.top = y + "px";
+  wamContainer.style.left = x + "px";
+  wamContainer.style.transform += ` scale(${scale})`;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  const container = document.querySelector('div');
+  if (container) {
+    container.style.width = windowWidth + "px";
+    container.style.height = windowHeight + "px";
+  }
+}
